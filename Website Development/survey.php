c89ad30d969bc957a59b035e776c63fecc321f00
JohnHew <johnjhewitt2002@gmail.com>
2023-03-06 14:44:38 +0000

    php

--- Website Development/survey.php
+++ Website Development/survey.php
@@ -1,7 +1,16 @@
 <?php
-    require('db.php');
-    $array = json_decode($_POST['listArray']);
-    $surveyname = $_GET['survey-title'];
-    $response = listArray();
-    echo($surveyname);
+    $con = mysqli_connect("localhost","2112834","85rj5j","db2112834");
+    // Check connection
+    if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    function(){
+        $data = json_decode($_POST['jObject'], true);
+        $surveyname = $_GET['survey-title'];
+        var_dump(isset($surveyname));
+        var_dump(isset($data));
+        $sql = "INSERT INTO `SurveyDetails` (`SurveyID`, `SurveyName`, `CreationDate`, `UploadStatus`, `UploadDate`, `Users_UserID`) VALUES (NULL, '$surveytitle', 'now()', 'Y', NULL, '2023502400')";
+        //$sql = "INSERT INTO `SurveyQuestions` (`QuestionNumber`, `QuestionName`, `QuestionType`, `QuestionStatus`, `SurveyDetails_SurveyID`) VALUES (' ', ' ', ' ', ' ', ' ')";
+        mysqli_connect($con, $sql);
+    }
 ?>
