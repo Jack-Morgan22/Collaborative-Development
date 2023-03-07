c3b6941bb0106828f87ff66decbf19a760603fe7
Sapphire Dixon <92919547+2113455@users.noreply.github.com>
2023-03-07 13:10:16 +0000

    Add files via upload
    Final version of the login form

--- Website Development/Versions/login.php
+++ Website Development/Versions/login.php
@@ -88,8 +88,8 @@ h3 {
 ?>
     <form class="form" method="post" name="login">
         <h1 class="login-title">Login</h1>
-        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
-        <input type="password" class="login-input" name="password" placeholder="Password"/>
+        <input type="text" class="login-input" name="Username" placeholder="Username" autofocus="true"/>
+        <input type="password" class="login-input" name="Password" placeholder="Password"/>
         <input type="submit" value="Login" name="submit" class="login-button"/>
         <p class="link"><a href="registration.php">New Registration</a></p>
   </form>
