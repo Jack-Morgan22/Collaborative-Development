c4a4361752177ca13c806f88799a17fe37f0b8a1
Sapphire Dixon <92919547+2113455@users.noreply.github.com>
2023-04-29 20:42:04 +0100

    Delete db.php

--- Website Development/Website MMP/View Surveys Page/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // This is where the connection happens through host name, database username, password, and database name.
-    $con = mysqli_connect("localhost","2112834","85rj5j","db2112834");
-    // The database will check connection between the site and database.
-    if (mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-?>
