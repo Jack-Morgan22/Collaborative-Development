d966341cf493dc85b8d68d6747cc0a8d6139dee8
JohnHew <johnjhewitt2002@gmail.com>
2023-05-08 12:53:26 +0100

    Create my-api4.php

---
+++ Website Development/Upload to Mi-LInux/New Uploads 8th May/my-api4.php
@@ -0,0 +1,36 @@
+<?php
+
+    $conn = mysqli_connect("localhost", "2112834", "85rj5j", "db2112834");
+    if(!$conn){
+        echo "Connection error," . mysqli_connect_error();
+    }
+    $username = stripslashes($_SESSION['Username']);
+
+    $sql1 = "SELECT UserID FROM `Users` WHERE Username = '$username'";
+
+    $result1 = mysqli_query($con, $sql1);
+    
+    while($rows = mysqli_fetch_assoc($result1)){
+        $userID = $rows['UserID'];
+    }
+
+    $sql = 'SELECT * FROM `SurveyDetails` WHERE User_UserID = '$userID'';
+
+    $result = mysqli_query($conn, $sql);
+
+    while($row = mysqli_fetch_assoc($result)){
+        $json[] = $row;
+    }
+
+    // Error ?
+    if($result == null) {
+        echo("<h4>SQL error description: " . $mysqli -> error . "</h4>");
+    }
+
+    // Get data, convert to JSON and print
+    echo json_encode($json);
+
+    // Free result set and close connection
+    $result -> free_result();
+    $conn -> close();
+    ?>
