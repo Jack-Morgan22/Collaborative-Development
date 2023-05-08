bc15fbccb7c8b973f574594a7a33ed9aeee3bc21
Jack-Morgan22 <jackmorgan251102@yahoo.com>
2023-05-08 14:44:11 +0100

    Jack's Mi-Linux files

---
+++ Website Development/Jack Morgan/my-api4.php
@@ -0,0 +1,29 @@
+<?php
+
+    $conn = mysqli_connect("localhost", "2112834", "85rj5j", "db2112834");
+    if(!$conn){
+        echo "Connection error," . mysqli_connect_error();
+    }
+
+    $userID = htmlspecialchars($_GET['userid']);
+
+    $sql = "SELECT * FROM `SurveyDetails` WHERE Users_UserID = '$userID'";
+
+    $result = mysqli_query($conn, $sql);
+
+    while($row = mysqli_fetch_assoc($result)){
+        $json[] = $row;
+    }
+
+    // Error ?
+    if($result == null) {
+        echo("<h4>SQL error description: " . $mysqli -> error . "</h4>");
+    }
+
+    // Get data, convert to JSON and print
+    echo json_encode($json);
+
+    // Free result set and close connection
+    $result -> free_result();
+    $conn -> close();
+    ?>
