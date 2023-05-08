e71cc973faa62133de33a5d3471b3e28c24e1d05
Sapphire Dixon <92919547+2113455@users.noreply.github.com>
2023-05-08 13:38:24 +0100

    Add files via upload

--- Website Development/Sapphire/dashboard.php
+++ Website Development/Sapphire/dashboard.php
@@ -75,19 +75,7 @@ if(!isset($_SESSION["Username"])){
             </form>
         </body>
         <script type="text/javascript">
-          var UserID = <?php require('db.php');
-          $username = stripslashes($_SESSION['Username']);
-
-          $sql1 = "SELECT UserID FROM `Users` WHERE Username = '$username'";
-
-          $result1 = mysqli_query($con, $sql1);
-
-          while($rows = mysqli_fetch_assoc($result1)){
-              $userID = $rows['UserID'];
-          }
-          echo($userID);
-          ?>
-            fetch('https://mi-linux.wlv.ac.uk/~2201053/Survey4All/my-api4.php?userid=' + UserID)
+            fetch('https://mi-linux.wlv.ac.uk/~2201053/Survey4All/my-api4.php')
             .then(response => response.json())
             .then(response => {
                 const questionList = document.getElementById("list");
